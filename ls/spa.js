9f22bc8f5981ac1fdddf2e1dabcba47b49d505ed
rivka <rivkamiller@hilma.tech>
2024-10-13 15:08:06 +0300

    show content func for spa works

--- ls/spa.js
+++ ls/spa.js
@@ -1,18 +1,19 @@
 let currTemp;
 
-function showContent(tempName) {
+function showContent(tempId) {
     //remove last
-    if (currTemp) {document.body.removeChild(currTemp)}
+    if (currTemp) {document.getElementsByClassName("tempShow")[0].remove()}
     console.log('currTemp: ', currTemp);
     
     //add new
-    let temp = document.getElementById(tempName);
+    let temp = document.getElementById(tempId);
     let clon = temp.content.cloneNode(true); 
     document.body.appendChild(clon);
     currTemp = temp;
 
 };
 
-
 showContent("login");
-showContent("addRevange");
+showContent("revenges");
+document.getElementById("newRevengeButton").addEventListener('click', () => showContent("addRevenge"));
+//document.getElementById("submitAddRevenge").addEventListener('click', () => showContent("revenges"));
