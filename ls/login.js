a05d7daa3be6c927da2f1739f45cf150e4e6b2b5
shirel-maymon <shirelmaymon@hilma.tech>
2024-10-13 16:35:58 +0300

    hi

--- ls/login.js
+++ ls/login.js
@@ -1,2 +1,29 @@
+const users = [
+    { username: 'admin', password: '12345' },
+    { username: 'user', password: 'password' }
+];
 
+if (localStorage.getItem('loggedIn')) {
+    alert("You are already logged in!");
+}
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (user) {
+        localStorage.setItem('loggedIn', 'true');
+        localStorage.setItem('username', username);
+
+        alert('Login successful!');
+        window.location.href = 'welcome.html'; 
+    } else {
+        errorMessage.style.display = 'block';
+    }
+});
 
--- ls/signup.js
+++ ls/signup.js
@@ -1,53 +1,42 @@
-const signButton = document.getElementById("signIn");
+// const signButton = document.getElementById("signIn");
 
 // construction function
-function SignInfo(username, password, password2) {
+function SignInfo(username, password, passwordd) {
   this.username = username;
   this.password = password;
-  this.password2 = password2;
+  this.passwordd = passwordd;
 }
 
-signButton.addEventListener("click", signIn);
-
 function signIn() {
-  const password2 = document.getElementById("password2").value;
   const username = document.getElementById("usernameInput").value;
   const password = document.getElementById("password").value;
+  const passwordd = document.getElementById("passwordd").value;
 
-  var usersArr = JSON.parse(window.localStorage.getItem("users"));
 
-  const aviCohen = {
-    username: "aviCohen",
-    password: 123,
-    first: "Avi",
-    last: "Cohen",
-    age: 18,
-    email: "avicohen@gmail.com",
-    score: 0,
-  };
+  //   var usersArr = JSON.parse(window.localStorage.getItem("users"));
 
-  console.log(usersArr);
+  //   console.log(usersArr);
 
-  for (let i = 0; i < usersArr.length; i++) {
-    if (username === usersArr[i]["username"]) {
-      document.getElementById("change").innerHTML =
-        "This username is taken. Please pick another one";
-      document.getElementById("usernameInput").style.border = "1px solid red";
-      return;
-    }
+  //   for (let i = 0; i < usersArr.length; i++) {
+  //     if (username === usersArr[i]["username"]) {
+  //       document.getElementById("change").innerHTML =
+  //         "This username is taken. Please pick another one";
+  //       document.getElementById("usernameInput").style.border = "1px solid red";
+  //       return;
+  //     }
 
-    const newUser = new SignInfo(
-      firstname,
-      lastname,
-      username,
-      password,
-      age,
-      email
-    );
-    usersArr.push(newUser);
-    console.log(usersArr);
-    window.localStorage.setItem("users", JSON.stringify(usersArr));
-    window.localStorage.setItem("currentUser", newUser);
-    window.location.href = "../index.html";
-  }
+  //     const newUser = new SignInfo(
+  //       firstname,
+  //       lastname,
+  //       username,
+  //       password,
+  //       age,
+  //       email
+  //     );
+  //     usersArr.push(newUser);
+  //     console.log(usersArr);
+  //     window.localStorage.setItem("users", JSON.stringify(usersArr));
+  //     window.localStorage.setItem("currentUser", newUser);
+  //     window.location.href = "../index.html";
+  //   }
 }
