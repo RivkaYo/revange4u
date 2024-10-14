8d163bedbd174230fdea634d6f4177fc8b92cfc3
rivka <rivkamiller@hilma.tech>
2024-10-14 14:14:03 +0300

    Revert "adds new revenges to revenges"
    This reverts commit 32580e959532cb1a1f9e6fe7627b5bfc0b10b4ba.

--- ls/revenges.js
+++ ls/revenges.js
@@ -1,5 +1,4 @@
 //Show template
-
 function showContent(tempId) {
     //remove last
     if (currTemp) {document.getElementsByClassName("tempShow")[0].remove()}
@@ -14,32 +13,8 @@ function showContent(tempId) {
 document.getElementById("newRevengeButton").addEventListener('click', () => showContent("addRevenge"));
 
 
-revengeList = JSON.parse(localStorage.getItem("revenge"));
+x = JSON.parse(localStorage.getItem("revenge"));
 
-for (let i=0; i < revengeList.length; i++) {
-    document.getElementById("revengeList").innerHTML += `<div id=${i} class="singleRevenge"><ul>You neet to revenge ${revengeList[i].name} with ${revengeList[i].details}. <button id=${i} class = "completeRevengeBtn" type="button">Revenge completed!</button></ul></div> `
+for (let i=0; i>x.length; i++) {
+    document.getElementById("revengeList").innerHTML = "<li> name: " + x[i].name + "Revenge:" + x[i].details + "</li>";
 };
-
-
-const deleteRevenge = (event) => {
-    console.log("5");
-    deletedId = event.target.id;
-    console.log('event.target.id: ', event.target.id);
-    document.querySelectorAll('.singleRevenge').forEach(div => {
-        
-        if (div.id === event.target.id) {
-            //remove from display
-            document.getElementById("revengeList").removeChild(div);
-
-            //remove from ls
-            console.log('event.target.id: ', event.target.id);
-            console.log('revenge.indexOf(event.target.id): ', revengeList.indexOf(event.target));
-            const checkStay = () => {return !revengeList.indexOf(event.target) === event.target};
-            updatedRevengeList = JSON.stringify(revengeList.filter(checkStay));
-            localStorage.setItem("revenge", updatedRevengeList);
-        };
-    });
-};
-
-Array.from(document.getElementsByClassName("completeRevengeBtn")).forEach(btn => {btn.addEventListener('click', (event) => deleteRevenge(event))});
-
