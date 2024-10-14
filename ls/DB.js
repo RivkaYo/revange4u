a78e35ce0500e3de4bd3fcc1347ed1b2615982f3
shirel-maymon <shirelmaymon@hilma.tech>
2024-10-14 15:18:52 +0300

    db2

--- ls/DB.js
+++ ls/DB.js
@@ -1,14 +1,26 @@
 // log in !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 const checkUser = () => {
-    //checks if username and pasword exised in ls users.
+ // טוען את המשתמשים מ-localStorage
+ const usersArr = JSON.parse(localStorage.getItem("users")) || [];
+ const currentUser = usersArr.find((user)=>    
+ user.username === username &&
+ user.password === password)
+
+ // תשנה את המשמש הנוכחי 
+ window.localStorage.removeItem("currentUser");
+ window.localStorage.setItem("currentUser", JSON.stringify(currentUser)); // עדכן את המשתמש הנוכחי ב-localStorage
 }
 
 
 //Signup ---------------------------------------------------------------------------
 
 const addUser = () => {
-    //asss user to ls
+    // אם המשתמש לא קיים, הוסף אותו
+  const newUser = new SignInfo(username, password);
+  users.push(newUser); // הוסף את המשתמש החדש למערך
+  window.localStorage.setItem("users", JSON.stringify(users)); // עדכן את ה-localStorage עם המערך החדש
+  window.localStorage.setItem("currentUser", JSON.stringify(newUser)); // עדכן את המשתמש הנוכחי
 }
 
 
