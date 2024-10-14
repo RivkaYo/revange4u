332c31948501b2512261ea1ee5e672084db02ff4
shirel-maymon <shirelmaymon@hilma.tech>
2024-10-14 15:06:18 +0300

    db

---
+++ ls/DB.js
@@ -0,0 +1,31 @@
+// log in !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+const checkUser = () => {
+    //checks if username and pasword exised in ls users.
+}
+
+
+//Signup ---------------------------------------------------------------------------
+
+const addUser = () => {
+    //asss user to ls
+}
+
+
+//Revenges -------------------------------------------------------------------------
+
+const displayRevenges = () => {
+    //displays all revenges from ls
+}
+
+const deleteRevenge = () => {
+    //deleteRevenge
+}
+
+
+
+//New revenges ---------------------------------------------------------------------
+
+const addRevenge = () => {
+    //adds Revenge to local storage
+}
