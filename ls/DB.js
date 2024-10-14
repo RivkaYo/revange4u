b94b15592679a063f948184a68f32d4dbcc949d4
shirel-maymon <shirelmaymon@hilma.tech>
2024-10-14 18:52:41 +0300

    fake

--- ls/DB.js
+++ ls/DB.js
@@ -1,70 +1,68 @@
 // log in -------------------------------------------------------------------------
 
 const checkUser = () => {
- // טוען את המשתמשים מ-localStorage
- const usersArr = JSON.parse(localStorage.getItem("users")) || [];
- const currentUser = usersArr.find((user)=>    
- user.username === username &&
- user.password === password)
+  // טוען את המשתמשים מ-localStorage
+  const usersArr = JSON.parse(localStorage.getItem("users")) || [];
+  const currentUser = usersArr.find(
+    (user) => user.username === username && user.password === password
+  );
 
- // תשנה את המשמש הנוכחי 
- window.localStorage.removeItem("currentUser");
- window.localStorage.setItem("currentUser", JSON.stringify(currentUser)); // עדכן את המשתמש הנוכחי ב-localStorage
-}
-
+  // תשנה את המשמש הנוכחי
+  window.localStorage.removeItem("currentUser");
+  window.localStorage.setItem("currentUser", JSON.stringify(currentUser)); // עדכן את המשתמש הנוכחי ב-localStorage
+};
 
 // Signup ---------------------------------------------------------------------------
 
 const addUser = () => {
-    // אם המשתמש לא קיים, הוסף אותו
+  // אם המשתמש לא קיים, הוסף אותו
   const newUser = new SignInfo(username, password);
   users.push(newUser); // הוסף את המשתמש החדש למערך
   window.localStorage.setItem("users", JSON.stringify(users)); // עדכן את ה-localStorage עם המערך החדש
   window.localStorage.setItem("currentUser", JSON.stringify(newUser)); // עדכן את המשתמש הנוכחי
-}
-
+};
 
 // Revenges -------------------------------------------------------------------------
 
 const displayRevenges = () => {
-    //displays all revenges from ls
-    revengeList = JSON.parse(localStorage.getItem("revenge"));
+  //displays all revenges from ls
+  revengeList = JSON.parse(localStorage.getItem("revenge"));
 
-    for (let i=0; i < revengeList.length; i++) {
-        document.getElementById("revengeList").innerHTML += `<div class="singleRevenge"><ul>You need to revenge ${revengeList[i].name} with ${revengeList[i].details}. <button onclick="deleteitem()" class = "completeRevengeBtn" type="button">Revenge completed!</button></ul></div> `
-    };
-}
+  for (let i = 0; i < revengeList.length; i++) {
+    document.getElementById(
+      "revengeList"
+    ).innerHTML += `<div class="singleRevenge"><ul>You need to revenge ${revengeList[i].name} with ${revengeList[i].details}. <button onclick="deleteitem()" class = "completeRevengeBtn" type="button">Revenge completed!</button></ul></div> `;
+  }
+};
 
 const deleteRevenge = (event) => {
-    //deleteing revenges - dosen't work yet
-    if (div.id === event.target.id) {
-        //remove from display
-        document.getElementById("revengeList").removeChild(div);
-    }
-}
-
-
+  //deleteing revenges - dosen't work yet
+  if (div.id === event.target.id) {
+    //remove from display
+    document.getElementById("revengeList").removeChild(div);
+  }
+};
 
 // New revenges ---------------------------------------------------------------------
 
 const addRevenge = () => {
-    //adds Revenge to local storage
-    let revengeName = document.querySelector("#name");
-    let revengeDetails = document.querySelector("#revengeDetails");
+  //adds Revenge to local storage
+  let revengeName = document.querySelector("#name");
+  let revengeDetails = document.querySelector("#revengeDetails");
 
-    let revengeNameValue = revengeName.value;
-    let revengeDetailsValue = revengeDetails.value;
-    //creats new revenge instince 
-    let currRevenge = new revenge(revengeNameValue, revengeDetailsValue);
+  let revengeNameValue = revengeName.value;
+  let revengeDetailsValue = revengeDetails.value;
+  //creats new revenge instince
+  let currRevenge = new revenge(revengeNameValue, revengeDetailsValue);
 
-    //get parsed revenge list
-    let revengeList = localStorage.getItem("revenge");
-    let revengeListParsed = JSON.parse(revengeList);
-    
-    //add instince to array
-    revengeListParsed.push(currRevenge);
+  //get parsed revenge list
+  let revengeList = localStorage.getItem("revenge");
+  let revengeListParsed = JSON.parse(revengeList);
 
-    //upload stringed list
-    let revengeListStringed = JSON.stringify(revengeListParsed);
-    localStorage.setItem("revenge", revengeListStringed);
+  //add instince to array
+  revengeListParsed.push(currRevenge);
+
+  //upload stringed list
+  let revengeListStringed = JSON.stringify(revengeListParsed);
+  localStorage.setItem("revenge", revengeListStringed);
 };
