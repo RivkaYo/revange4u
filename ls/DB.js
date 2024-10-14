cd7c7e2df176ac7cddf7574b5f53645ef4413025
rivka <rivkamiller@hilma.tech>
2024-10-14 15:13:04 +0300

    add bd revenge

--- ls/DB.js
+++ ls/DB.js
@@ -1,31 +1,58 @@
-// log in !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// log in -------------------------------------------------------------------------
 
 const checkUser = () => {
     //checks if username and pasword exised in ls users.
 }
 
 
-//Signup ---------------------------------------------------------------------------
+// Signup ---------------------------------------------------------------------------
 
 const addUser = () => {
     //asss user to ls
 }
 
 
-//Revenges -------------------------------------------------------------------------
+// Revenges -------------------------------------------------------------------------
 
 const displayRevenges = () => {
     //displays all revenges from ls
+    revengeList = JSON.parse(localStorage.getItem("revenge"));
+
+    for (let i=0; i < revengeList.length; i++) {
+        document.getElementById("revengeList").innerHTML += `<div class="singleRevenge"><ul>You neet to revenge ${revengeList[i].name} with ${revengeList[i].details}. <button onclick="deleteitem()" class = "completeRevengeBtn" type="button">Revenge completed!</button></ul></div> `
+    };
 }
 
-const deleteRevenge = () => {
-    //deleteRevenge
+const deleteRevenge = (event) => {
+    //deleteing revenges - dosen't work yet
+    if (div.id === event.target.id) {
+        //remove from display
+        document.getElementById("revengeList").removeChild(div);
+    }
 }
 
 
 
-//New revenges ---------------------------------------------------------------------
+// New revenges ---------------------------------------------------------------------
 
 const addRevenge = () => {
     //adds Revenge to local storage
-}
+    let revengeName = document.querySelector("#name");
+    let revengeDetails = document.querySelector("#revengeDetails");
+
+    let revengeNameValue = revengeName.value;
+    let revengeDetailsValue = revengeDetails.value;
+    //creats new revenge instince 
+    let currRevenge = new revenge(revengeNameValue, revengeDetailsValue);
+
+    //get parsed revenge list
+    let revengeList = localStorage.getItem("revenge");
+    let revengeListParsed = JSON.parse(revengeList);
+    
+    //add instince to array
+    revengeListParsed.push(currRevenge);
+
+    //upload stringed list
+    let revengeListStringed = JSON.stringify(revengeListParsed);
+    localStorage.setItem("revenge", revengeListStringed);
+};
--- ls/revenges.js
+++ ls/revenges.js
@@ -18,3 +18,13 @@ revengeList = JSON.parse(localStorage.getItem("revenge"));
 for (let i=0; i < revengeList.length; i++) {
     document.getElementById("revengeList").innerHTML += `<div class="singleRevenge"><ul>You neet to revenge ${revengeList[i].name} with ${revengeList[i].details}. <button onclick="deleteitem()" class = "completeRevengeBtn" type="button">Revenge completed!</button></ul></div> `
 };
+
+// //deleteing revenges
+// const deleteRevenge = (event) => {
+//     if (div.id === event.target.id) {
+//         //remove from display
+//         document.getElementById("revengeList").removeChild(div);
+//     }
+// }
+
+// Array.from(document.getElementsByClassName("completeRevengeBtn")).forEach(btn => {btn.addEventListener('click', (event) => deleteRevenge(event))});
