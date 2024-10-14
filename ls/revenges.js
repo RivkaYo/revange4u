7ed5e26b831921620dd390ccea3ff5304b67eba0
rivka <rivkamiller@hilma.tech>
2024-10-14 12:16:06 +0300

    many things dont work, this is just to hold edit

--- ls/revenges.js
+++ ls/revenges.js
@@ -17,5 +17,29 @@ document.getElementById("newRevengeButton").addEventListener('click', () => show
 revengeList = JSON.parse(localStorage.getItem("revenge"));
 
 for (let i=0; i < revengeList.length; i++) {
-    document.getElementById("revengeList").innerHTML += `<div class="singleRevenge"><ul>You neet to revenge ${revengeList[i].name} with ${revengeList[i].details}. <button onclick="deleteitem()" class = "completeRevengeBtn" type="button">Revenge completed!</button></ul></div> `
+    document.getElementById("revengeList").innerHTML += `<div id=${i} class="singleRevenge"><ul>You neet to revenge ${revengeList[i].name} with ${revengeList[i].details}. <button id=${i} class = "completeRevengeBtn" type="button">Revenge completed!</button></ul></div> `
 };
+
+
+const deleteRevenge = (event) => {
+    console.log("5");
+    deletedId = event.target.id;
+    console.log('event.target.id: ', event.target.id);
+    document.querySelectorAll('.singleRevenge').forEach(div => {
+        
+        if (div.id === event.target.id) {
+            //remove from display
+            document.getElementById("revengeList").removeChild(div);
+
+            //remove from ls
+            console.log('event.target.id: ', event.target.id);
+            console.log('revenge.indexOf(event.target.id): ', revengeList.indexOf(event.target));
+            const checkStay = () => {return !revengeList.indexOf(event.target) === event.target};
+            updatedRevengeList = JSON.stringify(revengeList.filter(checkStay));
+            localStorage.setItem("revenge", updatedRevengeList);
+        };
+    });
+};
+
+Array.from(document.getElementsByClassName("completeRevengeBtn")).forEach(btn => {btn.addEventListener('click', (event) => deleteRevenge(event))});
+
