e378d1451f417cc55c39aa9ba48568ae31d2f544
shirel-maymon <shirelmaymon@hilma.tech>
2024-10-15 10:47:46 +0300

    buttons

---
+++ ls/button.js
@@ -0,0 +1,5 @@
+const LogInButtonHeader = document.getElementById("loginbutton");
+LogInButtonHeader.addEventListener("click",  () => showContent("login"));
+
+const SignUpButtonHeader = document.getElementById("signupbutton");
+SignUpButtonHeader.addEventListener("click",  () => showContent("signUp"));
